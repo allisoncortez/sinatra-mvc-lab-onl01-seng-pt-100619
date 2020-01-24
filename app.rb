c219dd30426e036177cac5b08f712018f059d68d
allison cortez <allizetorc@gmail.com>
2020-01-24 03:00:34 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,10 +1,16 @@
+
 require_relative 'config/environment'
+require_relative 'models/piglatinizer.rb'
 
 class App < Sinatra::Base
   get '/' do
     erb :user_input
   end
   
+  get '/piglatinize' do
+    "Hello World"
+  end
+
   post '/piglatinize' do
     pl = PigLatinizer.new
     @piglatin = pl.piglatinize(params[:user_phrase])
@@ -12,6 +18,5 @@ class App < Sinatra::Base
     erb :results
     
   end
-  
-  
+
 end
